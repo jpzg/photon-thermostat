3a4e4b2252af286265143a16a2c475ebade5df2d
jpzg <jacobgrossbard@gmail.com>
2016-12-21 20:41:15 -0500

    Clean up thermostat.ino
    Removed some unused stuff and a bunch of things I didn't realize were
    added after my botched merge attempt.

--- thermostat.ino
+++ thermostat.ino
@@ -17,24 +17,9 @@ float night_limits[2] = {60.0,65.0};
 float * limits; // Turns out this pointer stuff might be necessary, and I <3 pointers. 
 
 bool active = false;
-bool needCooling; // Not used yet.
-bool needHeating;
 
 TCPClient client;
 byte server[] = {192,168,1,11};
-/*
-bool publish_data(String name, String data){ // Make fricking sure the events get published
-    while(!Particle.publish(name,data)){
-        delay(1000);
-    }
-    return true;
-}
-bool publish_status(String name){
-    while(!Particle.publish(name)){
-        delay(1000);
-    }
-}
-*/ // A buildup of these calls might be causing the "doesn't turn on properly" behavior that I'm seeing right now.
 
 void update_rht(){
     while(!rht.update()){
@@ -67,17 +52,11 @@ void loop(void) {
         RGB.color(255,0,0); // Red at night
     }
     
-<<<<<<< HEAD
     update_rht();
     tmp = rht.tempF();
     hum = rht.humidity();
     
     if(tmp < *limits){
-=======
-    float tmp = rht.tempF();
-    float hum = rht.humidity();
-    if(tmp < 66.0){
->>>>>>> 0a5fe20802719c24ebb2bb8c1fb3b86a55f631f6
         digitalWrite(heat,HIGH);
         digitalWrite(fan,HIGH);
         digitalWrite(D7,HIGH);
@@ -89,7 +68,6 @@ void loop(void) {
         Particle.publish("temperature",String(tmp));
         Particle.publish("humidity",String(hum));
     }
-<<<<<<< HEAD
     // Stays in this loop while the heat is on
     while((tmp < *(limits+1)) & active){ // Adding one to the limits pointer before dereferencing is the same as limits[1] would be in Python; the next element is in the next spot in memory.
         delay(5*1000); // Slow it down. 5s between readings.
@@ -99,21 +77,6 @@ void loop(void) {
         Particle.publish("temperature",String(tmp));
         Particle.publish("humidity",String(hum));
         client.print("tmp: " + String(tmp));
-=======
-    // Could probably move everything above this to setup(), and remove the while loop
-    while(tmp < 66.0){ // All this shit needs to be cleaned. Also, it doesn't seem to stop at 68, so I'm lowering it to 62. May need to add more temperature sensors.
-        int ret = rht.update();
-        if(ret == 1){
-            tmp = rht.tempF();
-            hum = rht.humidity();
-            publish_data("temperature",String(tmp));
-            publish_data("humidity",String(hum));
-        }
-        else{
-            delay(RHT_READ_INTERVAL_MS);
-        }
-        delay(10*1000); // Slow it down. 10s between readings.
->>>>>>> 0a5fe20802719c24ebb2bb8c1fb3b86a55f631f6
     }
     if(active){
         digitalWrite(relay,HIGH); // Cuts power for everything so the TRIACs can turn off (not needed if wiring uses)
@@ -126,19 +89,6 @@ void loop(void) {
         Particle.publish("heat-off");
         client.print("Turning off heat");
     }
-<<<<<<< HEAD
     delay(5*1000);
-    /* Doesn't need to SLEEP_MODE_DEEP anymore; it's not on battery
-    if(Time.hour() > 21){ // Basic scheduling
-        System.sleep(SLEEP_MODE_DEEP,8*3600); // Go to bed for the night (10PM-6AM)
-    }
-    if(Time.hour() > 9){
-        System.sleep(SLEEP_MODE_DEEP,5*3600); // Stay off during the day (9AM-2PM)
-    }*/
     client.print("tmp: " + String(tmp));
-=======
-    delay(10*1000); // Sleep for ten seconds. Maybe this will help it publish like it's supposed to?
-    System.sleep(SLEEP_MODE_DEEP,5*60); // Deep sleep for 5min, then reset. Consumes a few uA in this mode. The publishes aren't working, so I'm disabling this for the moment.
-    //delay(10*1000);
->>>>>>> 0a5fe20802719c24ebb2bb8c1fb3b86a55f631f6
 }
